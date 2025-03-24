cc1468440452cdb12c1955cb84731ce137061c9f
keisuke_oyama <oyama.keisuke@alhinc.jp>
2025-03-24 11:24:56 +0900

    addiction new function of lesson 3

--- src/chapter6/controller/SettingServlet.java
+++ src/chapter6/controller/SettingServlet.java
@@ -111,9 +111,11 @@ public class SettingServlet extends HttpServlet {
 		}.getClass().getEnclosingClass().getName() + " : " + new Object() {
 		}.getClass().getEnclosingMethod().getName());
 
+		Integer id = user.getId();
 		String name = user.getName();
 		String account = user.getAccount();
 		String email = user.getEmail();
+		User existingAccount = new UserService().select(account);
 
 		if (!StringUtils.isEmpty(name) && (20 < name.length())) {
 			errorMessages.add("名前は20文字以下で入力してください");
@@ -122,6 +124,9 @@ public class SettingServlet extends HttpServlet {
 			errorMessages.add("アカウント名を入力してください");
 		} else if (20 < account.length()) {
 			errorMessages.add("アカウント名は20文字以下で入力してください");
+		}
+		if (existingAccount != null && existingAccount.getId() != id) {
+			errorMessages.add("すでに存在するアカウントです");
 		}
 		if (!StringUtils.isEmpty(email) && (50 < email.length())) {
 			errorMessages.add("メールアドレスは50文字以下で入力してください");
